4c517aa4acec9e4f4c12fb8b8ab01aee073ae248
sahiltapu <sahilmishranayagarh@gmail.com>
2024-02-06 20:26:44 +0530

    first commit

---
+++ src/components/Layouts/Layout.jsx
@@ -0,0 +1,17 @@
+import React, { Children } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+const Layout = ({Children}) => {
+  return (
+    <>
+    <Header />
+    <div>
+      {Children}
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+export default Layout
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../../components/Layouts/Layout'
+import Section1 from './Section1'
+import "../../styles/HomeStyle.css";
+
+const Home = () => {
+  return (
+    <>
+    <Layout />
+    <Section1 />
+    </>
+
+  )
+}
+
+export default Home
